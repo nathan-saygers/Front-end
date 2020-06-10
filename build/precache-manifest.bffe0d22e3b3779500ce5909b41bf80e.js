7f53073aef5577dff80698b722c2ed490c84dafa
nathan-saygers <nathan.saygers@gmail.com>
2020-06-10 12:23:01 -0700

    build dir result from npm run build

---
+++ build/precache-manifest.bffe0d22e3b3779500ce5909b41bf80e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70368b31fec4f7802065e01ab5f766ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b894e5c218eb07fa17d1",
+    "url": "/static/css/main.cffeeb8c.chunk.css"
+  },
+  {
+    "revision": "d102344c1e28dc4d3a9e",
+    "url": "/static/js/2.0e99e6c8.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.0e99e6c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b894e5c218eb07fa17d1",
+    "url": "/static/js/main.d108806f.chunk.js"
+  },
+  {
+    "revision": "86776912d83c93d98f79",
+    "url": "/static/js/runtime-main.09e203bb.js"
+  }
+]);
